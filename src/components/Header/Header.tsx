07d6215a2de457f3d7c43738d68892e921cb4f96
HasanovDoc <hasanov326@mail.ru>
2024-03-08 04:40:35 +0400

    added header secret

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,38 @@
+import { DragEvent, useEffect, useRef, useState } from 'react'
 import './Header.css'
 
+interface Position {
+    y: number;
+}
 
 export default function Header() {
+    const [position, setPosition] = useState<Position>({ y: 0 });
+    const headerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (headerRef.current) {
+        setPosition({ y: headerRef.current.offsetTop });
+        }
+    }, []);
+
+    const dragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        event.dataTransfer.setData('text/plain', event.currentTarget.id);
+    };
+
+    const dragOver = (event: React.DragEvent<HTMLDivElement>) => {
+        event.preventDefault();
+        setPosition({ y: event.pageY });
+    };
+
     return (
         <div className='HeaderContainer'>
-
+            <div id="draggableHeader" className="HeaderWrapper" draggable="true"
+            onDragStart={dragStart}
+            onDragOver={dragOver}
+            // style={{position: 'absolute', top: position.y}}
+            style={{position: 'absolute', transform: `translateY(${position.y}px)`}}
+            ></div>
+            <h1 className='LoveTips'>Люблю тебя</h1>
         </div> 
     )
 }
